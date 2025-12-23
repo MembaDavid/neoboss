75ff84baefa286d9bc321f4ee844f6b3d559e2a4
MembaDavid <davidmemba2@gmail.com>
2025-12-23 23:58:10 +0300

    feat: add FAQ page with Header and FAQ components

---
+++ app/faq/page.tsx
@@ -0,0 +1,11 @@
+import FAQPage from "@/components/faq";
+import Header from "@/components/Header";
+
+export default function FAQ() {
+  return (
+    <main className="min-h-screen bg-white">
+      <Header />
+      <FAQPage />
+    </main>
+  );
+}
